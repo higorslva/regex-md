20563d299125f7097c6aa7e527fc978b41368aef
Higor Silva <higor.slva@outlook.com>
2024-06-27 10:44:43 -0300

    Add "Topo" support for remove

--- clean_recursive.py
+++ clean_recursive.py
@@ -3,18 +3,20 @@ import re
 import os
 
 def clean_markdown(content):
-    content = re.sub(r'\[(.*?)\]\((.*?)\)', r'\1 (\2)', content)    # Rremover colchetes e parênteses de links
-    content = re.sub(r'(#)', ' ', content)                          # Remover jogo da velha
-    content = re.sub(r'\n', ' ', content)                           # Remover "\n" do markdown
-    content = re.sub(r'#{1,6} ', '', content)                       # Remover cabeçalhos de markdown
-    content = re.sub(r'\*|_', '', content)                          # Remover asteriscos e sublinhados
-    content = re.sub(r'\*\*(.*?)\*\*', r'\1', content)              # Remover texto em negrito
-    content = re.sub(r'_(.*?)_', r'\1', content)                    # Remover texto em itálico
-    content = re.sub(r'`', '', content)                             # Remover outros caracteres especiais do Markdown
+    content = re.sub(r'\[(.*?)\]\((.*?)\)', r'\1 \2', content)           # Remover colchetes e parênteses de links
+    content = re.sub(r'(#)', ' ', content)                               # Remover jogo da velha
+    #content = re.sub(r'\n', ' ', content)                               # Remover "\n" do markdown
+    content = re.sub(r'#{1,6} ', '', content)                            # Remover cabeçalhos de markdown
+    content = re.sub(r'\*|_', '', content)                               # Remover asteriscos e sublinhados
+    content = re.sub(r'\*\*(.*?)\*\*', r'\1', content)                   # Remover texto em negrito
+    content = re.sub(r'_(.*?)_', r'\1', content)                         # Remover texto em itálico
+    content = re.sub(r'`', '', content)                                  # Remover outros caracteres especiais do Markdown
+    content = re.sub(r'^Topo.*?http://mail.tjap.jus.br/', '', content)   # Remover header
+    content = re.sub(r'Tribunal.*?feriados\.', '', content)              # Remover footer simples
     return content
 
-input_folder = 'jsons'
-output_folder = 'limpo'
+input_folder = 'testesLimpJSON/jsons'
+output_folder = 'testesLimpJSON/limpo'
 
 # Criar a pasta de saída se não existir
 os.makedirs(output_folder, exist_ok=True)
@@ -29,6 +31,7 @@ for filename in os.listdir(input_folder):
         with open(file_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         
+        del data['markdown']
         # Limpar o conteúdo
         data['content'] = clean_markdown(data['content'])
         
