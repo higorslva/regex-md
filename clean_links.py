bf9aca5496d246ca1786fe790cad4ddc2cdfa4bd
Katlen Vanessa <94211156+KatlenVanessa@users.noreply.github.com>
2024-06-27 22:56:49 -0300

    up

---
+++ clean_links.py
@@ -0,0 +1,61 @@
+import json
+import re
+import os
+
+def clean_markdown(content):
+    content = re.sub(r'\[(.*?)\]\((.*?)\)', r'\1 (\2)', content)           # Remover colchetes e parênteses de links
+    content = re.sub(r'(#)', ' ', content)                               # Remover jogo da velha
+    content = re.sub(r'\n', ' ', content)                               # Remover "\n" do markdown
+    content = re.sub(r'#{1,6} ', '', content)                            # Remover cabeçalhos de markdown
+    content = re.sub(r'\*|_', '', content)                               # Remover asteriscos e sublinhados
+    content = re.sub(r'\*\*(.*?)\*\*', r'\1', content)                   # Remover texto em negrito
+    content = re.sub(r'_(.*?)_', r'\1', content)                         # Remover texto em itálico
+    content = re.sub(r'`', '', content)                                  # Remover outros caracteres especiais do Markdown
+    content = re.sub(r'Tribunal de Justiça do Estado do Amapá - CNPJ.*', '', content)   # Remover footer simples
+    content = re.sub(r'Conheça o Tribunal.*', '', content)   # Remover footer de links
+    content = re.sub(r'Rua General Rondon.*', '', content)   # Remover footer 
+    content = re.sub(r'^Topo.*\(/portal/intranet\).*\(http://mail\.tjap\.jus\.br/\)', '', content)   # Remover header
+    content = re.sub(r'Mobile Menu Toggle.*\(http://mail\.tjap\.jus\.br/.*\"Webmail\"\)', '', content)  # Remover header
+    content = re.sub(r'.*\(http://mail\.tjap\.jus\.br/.*\"Webmail\"\)', '', content)  # Remover header
+    content = re.sub(r'\"Back to Top\"', '', content)  # Remover header
+    content = re.sub(r'\<br\>', '', content)  # Remover header
+    return content
+
+input_folder = 'links'
+output_folder = 'links\limpo'
+
+# Criar a pasta de saída se não existir
+os.makedirs(output_folder, exist_ok=True)
+
+# Processar todos os arquivos na pasta de entrada
+for filename in os.listdir(input_folder):
+    if filename.endswith('.json'):
+        # Caminho completo para o arquivo
+        file_path = os.path.join(input_folder, filename)
+        
+        # Ler o arquivo JSON
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+                
+        if data['metadata']['pageStatusCode'] != 403:
+            if 'markdown' in data:
+                del data['markdown']
+          
+            # Limpar o conteúdo
+            data['content'] = clean_markdown(data['content'])
+            
+            #Completa os links
+            url = data['metadata']['sourceURL']        
+            if url.startswith("https://www.", 0, 12):
+                data['content'] = re.sub(r'\((\/portal.*?)\)', r'(https://www.tjap.jus.br\1)', data['content'])
+            else:
+                data['content'] = re.sub(r'\((\/portal.*?)\)', r'(https://old.tjap.jus.br\1)', data['content'])
+            
+            # Caminho completo para o arquivo limpo
+            cleaned_file_path = os.path.join(output_folder, filename)
+            
+            # Salvar o conteúdo limpo em um novo arquivo JSON
+            with open(cleaned_file_path, 'w', encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
+
+print(f"Conteúdo limpo e salvo na pasta '{output_folder}'.")
