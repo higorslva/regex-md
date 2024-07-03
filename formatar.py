4f9372b8f5a4ae3157d038556df72f15f0af0dba
Katlen Vanessa <94211156+KatlenVanessa@users.noreply.github.com>
2024-07-03 10:56:35 -0300

    formatados

--- formatar.py
+++ formatar.py
@@ -2,21 +2,17 @@ import json
 import re
 import os
 
-# Função para extrair descrições e links do texto
-def extract_descriptions_and_links(content):
+def extract_options_and_links(content):
     pattern = re.compile(r'\[(.*?)\]\((.*?)\)')
     matches = pattern.findall(content)
     return matches
 
-# Função principal
 def main():
-    input_directory = 'formatar'  # Nome da pasta de entrada
-    output_directory = 'formatar/formatado'  # Nome da pasta de saída
+    input_directory = 'formatar'  
+    output_directory = 'formatar/formatado' 
     
-    # Cria a pasta de saída se ela não existir
     os.makedirs(output_directory, exist_ok=True)
     
-    # Lista todos os arquivos JSON na pasta de entrada
     for filename in os.listdir(input_directory):
         if filename.startswith('response_') and filename.endswith('.json'):
             input_path = os.path.join(input_directory, filename)
@@ -24,25 +20,26 @@ def main():
             
             with open(input_path, 'r', encoding='utf-8') as f:
                 data = json.load(f)
-                        
+            
             content = data.pop('content', '')
             metadata = data.pop('metadata', {})
             source_url = metadata.get('sourceURL', '')
             
-            # Extrair descrições e links
-            descriptions_and_links = extract_descriptions_and_links(content)
-            
-            # Criar a estrutura de saída
-            output_data = []
+            descriptions_and_links = extract_options_and_links(content)
+        
+            content = re.sub(r'\[(.*?)\]\((.*?)\)', r'', content)          
+
+            output_data = {
+                "opcao_e_link": [],
+                "sourceURL": source_url,
+                "descricao": content
+            }
             for description, link in descriptions_and_links:
-                output_data.append({
+                output_data["opcao_e_link"].append({
                     "opcao": description,
-                    "link": link,
-                    "sourceURL": source_url,
-                    "descrição": content
+                    "link": link
                 })
 
-            # Salvar o JSON de saída
             with open(output_path, 'w', encoding='utf-8') as f:
                 json.dump(output_data, f, ensure_ascii=False, indent=4)
 
